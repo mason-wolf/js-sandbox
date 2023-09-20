2bad50bf3ea28c57eb0c48b662106f1fe26c2b9d
Mason Wolf <masonhwolf@gmail.com>
2023-09-19 21:49:08 -0500

    Added grid system example

--- js/paginator-1.1.js
+++ js/paginator-1.1.js
@@ -1,11 +1,21 @@
+/**
+ * Creates an Paginator object.
+ */
 class Paginator {
+    // List of records or items.
     items;
+    // Max items per page.
     itemsPerPage = 10;
+    // Div container with paginated items.
     container;
+    // Div container with pagination controls.
     controls;
     currentPage = 1;
     pages;
 
+    /**
+     * Split items into pages.
+     */
     paginate() {
         this.pages = [];
         while (this.items.length > 0) {
@@ -14,6 +24,9 @@ class Paginator {
         this.changePage(this.currentPage);
     }
 
+    /**
+     * Previous page.
+     */
     previousPage() {
         if (this.currentPage != 1) {
             this.currentPage--;
@@ -21,6 +34,9 @@ class Paginator {
         }
     }
 
+    /**
+     * Next page.
+     */
     nextPage() {
         if (this.currentPage != this.pages.length) {
             this.currentPage++;
@@ -28,10 +44,16 @@ class Paginator {
         }
     }
 
+    /**
+     * Change page.
+     * @param {*} page_num Page Number
+     */
     changePage(page_num) {
         this.currentPage = page_num;
         let paginator = this.container;
         paginator.innerHTML = "";
+
+        // Append the item data to the container.
         for (var item in this.pages[this.currentPage - 1]) {
             paginator.innerHTML +=
                 `
@@ -40,14 +62,17 @@ class Paginator {
         }
         paginator.innerHTML += `<br>`;
 
+        // Add the controls.
         let controls = this.controls;
         controls.innerHTML = "";
 
+        // Previous button.
         controls.innerHTML +=
             `
         <button onclick='paginator.previousPage()' style='width:auto;'>Previous</button>
         `;
 
+        // Next button.
         controls.innerHTML +=
             `
         <button onclick='paginator.nextPage()'>Next</button>
